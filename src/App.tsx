7fac4dd7b48039f5938f6f00028eab78f70e58df
Logan Zartman <logan.zartman@utexas.edu>
2020-06-10 15:54:52 -0500

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Detail from "./Detail";
 import WebSocketTest from "./WebSocketTest";
 
 const serverUrl = "ws://localhost:8080";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>RoboFleet WebSocket Test</p>
-      </header>
-      <WebSocketTest url={serverUrl}/>
-    </div>
-  );
+  return <Router>
+    <Link to="/">Home</Link>
+    <Link to="/robot/1">Robot details</Link>
+    <Switch>
+      <Route exact path="/">
+        <WebSocketTest url={serverUrl}/>
+      </Route>
+      <Route exact path="/robot/:id">
+        <Detail/>
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
