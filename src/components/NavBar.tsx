c7b278afd7669daeddf9f9bc2d66ef01e5645d7f
Logan Zartman <logan.zartman@utexas.edu>
2020-06-11 02:14:17 -0500

    add connection indicator

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
 import { Home } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import WebSocketContext from "../contexts/WebSocketContext";
+import { Wifi, WifiOff } from "@material-ui/icons";
 
 export default function NavBar() {
+  const ws = useContext(WebSocketContext);
+  const connectionIndicator = ws?.state === "connected" ? <Wifi/> : <WifiOff color="disabled"/>;
+
   return <AppBar position="static" color="transparent">
     <Toolbar>
       <IconButton component={Link} to="/" edge="start" color="inherit" aria-label="home">
@@ -12,6 +17,7 @@ export default function NavBar() {
       <Typography variant="h6" style={{flexGrow: 1}}>
         Robofleet
       </Typography>
+      {connectionIndicator}
       <Button color="inherit">Login</Button>
     </Toolbar>
   </AppBar>;
