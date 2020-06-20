31e77260c425b2fdb34294ee9a82d677fab1158b
Logan Zartman <logan.zartman@utexas.edu>
2020-06-20 16:20:28 -0500

    improve imagery interface

--- src/ImageryTab.tsx
+++ src/ImageryTab.tsx
@@ -1,17 +1,82 @@
-import React from "react";
-import { Container, Box, Card, CardContent } from "@material-ui/core";
+import React, { useState } from "react";
+import { Container, Box, Card, CardContent, GridList, GridListTile, CardActionArea, CardMedia, Typography, Dialog, DialogTitle, DialogContent, IconButton } from "@material-ui/core";
 import CompressedImageViewer from "./components/CompressedImageViewer";
+import { Close } from "@material-ui/icons";
 
-export default function ImageryTab(props: {namespace: string}) {
-  return <Container maxWidth="md">
-    <Box height="2em"/>
-    <Card>
+export function ImageCard(props: {namespace: string, topic: string, enablePreviews?: boolean, onClose?: () => void, onOpen?: () => void}) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleOpen = () => {
+    setDialogOpen(true);
+    if (props.onOpen)
+      props.onOpen();
+  }
+  const handleClose = () => {
+    setDialogOpen(false);
+    if (props.onClose)
+      props.onClose();
+  }
+  const dialog = <Dialog maxWidth={false} onClose={handleClose} open={dialogOpen}>
+    <DialogTitle disableTypography>
+      <Typography variant="h6">{props.topic}</Typography>
+      <IconButton 
+        aria-label="close" 
+        onClick={handleClose}
+        style={{position: "absolute", top: "0.5rem", right: "0.5rem"}}
+      >
+        <Close/>
+      </IconButton>
+    </DialogTitle>
+    <DialogContent dividers>
+      <CompressedImageViewer
+        namespace={props.namespace}
+        topic={props.topic}
+        enabled={dialogOpen}
+      />
+    </DialogContent>
+  </Dialog>;
+
+  const card = <Card style={{maxWidth: "350px"}}>
+    <CardActionArea onClick={handleOpen}>
       <CardContent>
         <CompressedImageViewer
           namespace={props.namespace}
-          topic="stereo/left/image_raw/compressed"
+          topic={props.topic}
+          enabled={props.enablePreviews ?? false}
         />
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.topic}
+        </Typography>
       </CardContent>
-    </Card>
+    </CardActionArea>
+  </Card>;
+
+  return <>
+    {card}
+    {dialog}
+  </>;
+}
+
+export default function ImageryTab(props: {namespace: string}) {
+  const [enablePreviews, setEnablePreviews] = useState(true);
+
+  const topics = [
+    "stereo/left/image_raw/compressed",
+    "stereo/right/image_raw/compressed"
+  ];
+
+  const startPreviews = () => setEnablePreviews(true);
+  const stopPreviews = () => setEnablePreviews(false);  
+  return <Container maxWidth="lg">
+    <Box height="2em"/>
+    <Box display="flex" flexDirection="row" flexWrap="wrap" justifyContent="space-around">
+      {topics.map((topic) => <ImageCard 
+        namespace={props.namespace}
+        topic={topic}
+        enablePreviews={enablePreviews}
+        onOpen={stopPreviews}
+        onClose={startPreviews}
+      />)}
+    </Box>
   </Container>;
 }
--- src/components/CompressedImageViewer.tsx
+++ src/components/CompressedImageViewer.tsx
@@ -1,21 +1,24 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useLayoutEffect, ReactElement, useRef } from "react";
 import useRobofleetMsgListener from "../hooks/useRobofleetMsgListener";
 import { matchTopic } from "../util";
 import { fb } from "../schema";
 
-export default function CompressedImageViewer(props: {namespace: string, topic: string}) {
+export default function CompressedImageViewer(props: {namespace: string, topic: string, enabled: boolean}) {
   const { namespace } = props;
+  const img = useRef<HTMLImageElement>(null);
   const [blob, setBlob] = useState(null as Blob | null);
   const [imgSrc, setImgSrc] = useState("");
   
   useRobofleetMsgListener(matchTopic(namespace, props.topic), useCallback((buf, match) => {
+    if (!props.enabled)
+      return;
     const ci = fb.sensor_msgs.CompressedImage.getRootAsCompressedImage(buf);
     const blob = new Blob(
       [ci.dataArray() ?? new Uint8Array()], 
       {type: `image/${ci.format()}`}
     );
     setBlob(blob);
-  }, []));
+  }, [props.enabled]));
 
   useEffect(() => {
     if (blob === null)
@@ -27,5 +30,5 @@ export default function CompressedImageViewer(props: {namespace: string, topic: 
     };
   }, [blob]);
   
-  return <img src={imgSrc}/>; 
+  return <img ref={img} src={imgSrc} style={{maxWidth: "100%", maxHeight: "100%"}}/>; 
 }
