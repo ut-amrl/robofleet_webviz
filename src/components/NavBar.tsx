cb35034226596d352049e58ffc2e3b9c6fa571b4
Logan Zartman <logan.zartman@utexas.edu>
2020-06-23 14:57:31 -0500

    basic sign-in support (no auth to server)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,62 +1,16 @@
-import { AppBar, Divider, fade, IconButton, ListItemIcon, ListItemText, makeStyles, Menu, MenuItem, Theme, Toolbar, Tooltip, Typography } from "@material-ui/core";
-import { AccountCircle, PersonAdd, Wifi, WifiOff, WbIncandescent, WbIncandescentOutlined } from "@material-ui/icons";
-import React, { ReactElement, useContext, useState } from "react";
-import WebSocketContext from "../contexts/WebSocketContext";
-import useIpAddress from "../hooks/useIpAddress";
-import Logo from "./Logo";
+import { AppBar, fade, IconButton, makeStyles, Theme, Toolbar, Tooltip, Typography } from "@material-ui/core";
+import { WbIncandescent, WbIncandescentOutlined, Wifi, WifiOff } from "@material-ui/icons";
+import React, { ReactElement, useContext } from "react";
 import AppContext from "../contexts/AppContext";
+import WebSocketContext from "../contexts/WebSocketContext";
+import Logo from "./Logo";
+import UserProfileButton from "./UserProfileButton";
 
 const useStyles = makeStyles((theme: Theme) => ({
   toolbar: {
     backgroundColor: fade(theme.palette.background.paper, 0.67),
   },
 }));
-
-export function UserProfileButton() {
-  const ipAddr = useIpAddress();
-  const [anchor, setAnchor] = useState<HTMLElement | null>(null);
-
-  const handleClickOpen = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchor(event.currentTarget);
-  };
-
-  const doClose = () => {
-    setAnchor(null);
-  };
-
-  const profileMenu = <Menu
-    id="profile-menu"
-    anchorEl={anchor}
-    keepMounted
-    open={Boolean(anchor)}
-    onClose={doClose}
-  >
-    <MenuItem disabled>
-      <ListItemIcon>IP</ListItemIcon>
-      <ListItemText><code>{ipAddr ?? "<unknown>"}</code></ListItemText>
-    </MenuItem>
-    <Divider/>
-    <MenuItem onClick={doClose}>
-      <ListItemIcon><PersonAdd/></ListItemIcon>
-      <ListItemText>Sign in</ListItemText>
-    </MenuItem>
-  </Menu>;
-
-  return <>
-    <Tooltip arrow title="User Account">
-      <IconButton
-        edge="end"
-        aria-label="account of current user"
-        aria-haspopup="true"
-        onClick={handleClickOpen}
-        color="inherit"
-      >
-        <AccountCircle/>
-      </IconButton>
-    </Tooltip>
-    {profileMenu}
-  </>;
-}
 
 export default function NavBar(props: {title?: string, navIcon?: ReactElement<any>, tabs?: ReactElement<any>}) {
   const ws = useContext(WebSocketContext);
