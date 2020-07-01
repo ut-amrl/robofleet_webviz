6f1a9eb6115ca7605c2bb3f90a7859741c2315b1
Logan Zartman <logan.zartman@utexas.edu>
2020-07-01 16:57:42 -0500

    fix colored lines viz

--- src/components/VisualizationViewer.tsx
+++ src/components/VisualizationViewer.tsx
@@ -1,78 +1,73 @@
-import React, { useState, useCallback, useMemo, useEffect } from "react";
+import React, { useCallback, useMemo, useState } from "react";
+import { Color, Matrix4 } from "three";
 import useRobofleetMsgListener from "../hooks/useRobofleetMsgListener";
-import { matchTopic } from "../util";
 import { fb } from "../schema";
-import THREE, { BufferAttribute, Color, Vector4, Vector3, Matrix4 } from "three";
-
-const I = new Matrix4();
+import { matchTopic } from "../util";
 
 export function ColoredLinesViewer(props: 
-    {msg: fb.amrl_msgs.VisualizationMsg, frame?: string, matrix?: Matrix4}) {
-  const { msg, frame } = props;
-  const [linesLength, setLinesLength] = useState(0);
-  const [posArray, setPosArray] = useState(new Float32Array(1000000));
-  const [posAttr, setPosAttr] = useState<BufferAttribute>();
-  const [colArray, setColArray] = useState(new Float32Array(1000000));
-  const [colAttr, setColAttr] = useState<BufferAttribute>();
+    {msg: fb.amrl_msgs.VisualizationMsg, matrix?: Matrix4}) {
+  const { msg } = props;
 
-  // grow buffers as necessary
-  useEffect(() => {
-    if (frame && frame !== msg.header()?.frameId())
-      return;
-    const newLength = msg.linesLength();
-    if (newLength <= linesLength)
-      return;
+  const length = msg.linesLength();
 
-    setPosArray(new Float32Array(newLength * 2 * 3));
-    setPosAttr(new BufferAttribute(posArray, 3, false));
-
-    setColArray(new Float32Array(newLength * 2 * 3));
-    setColAttr(new BufferAttribute(colArray, 3, false));
-  }, [msg.linesLength()]);
-
-  // update buffer contents
-  useEffect(() => {
-    if (frame && frame !== msg.header()?.frameId())
-      return;
-    if (!posAttr || !colAttr)
-      return;
-
-    const a = new Vector3();
-    const b = new Vector3();
-    const color = new Color();
-
+  const positionAttrib = useMemo(() => {
+    const data = new Float32Array(length * 2 * 3);
     for (let i = 0; i < msg.linesLength(); ++i) {
+      const index = i * 2 * 3;
       const l = msg.lines(i)!;
-      const index = i * 2; // two vertices per line segment
-      
-      // both vertices get the same color
-      color.set(l.color());
-      colAttr.setXYZ(index + 0, color.r, color.g, color.b);
-      colAttr.setXYZ(index + 1, color.r, color.g, color.b);
 
-      a.set(l.p0()!.x(), l.p0()!.y(), 0);
-      b.set(l.p1()!.x(), l.p1()!.y(), 0);
-      posAttr.setXYZ(index + 0, a.x, a.y, a.z);
-      posAttr.setXYZ(index + 1, b.x, b.y, b.z);
+      // p0
+      data[index + 0] = l.p0()!.x();
+      data[index + 1] = l.p0()!.y();
+      data[index + 2] = 0;
+
+      // p1
+      data[index + 3] = l.p1()!.x();
+      data[index + 4] = l.p1()!.y();
+      data[index + 5] = 0;
     }
-    colAttr.needsUpdate = true;
-    posAttr.needsUpdate = true;
-  }, [msg, posAttr, colAttr]);
 
-  if (!posAttr || !colAttr)
-    return <></>;
+    return <bufferAttribute
+      attachObject={["attributes", "position"]}
+      args={[data, 3, false]}
+      count={length * 2}
+      onUpdate={(self) => self.needsUpdate = true}
+    />;
+  }, [msg]);
 
+  const colorAttrib = useMemo(() => {
+    const data = new Float32Array(length * 2 * 3);
+    const color = new Color();
+    for (let i = 0; i < msg.linesLength(); ++i) {
+      const index = i * 2 * 3;
+      const l = msg.lines(i)!;
+
+      // same color for both vertices
+      color.set(l.color());
+      data[index + 0] = data[index + 3] = color.r;
+      data[index + 1] = data[index + 4] = color.g;
+      data[index + 2] = data[index + 5] = color.b;
+    }
+
+    return <bufferAttribute
+      attachObject={["attributes", "color"]}
+      args={[data, 3, false]}
+      count={length * 2}
+      onUpdate={(self) => self.needsUpdate = true}
+    />;
+  }, [msg]);
+
+  const I = new Matrix4();
   return <lineSegments
     frustumCulled={false}
     matrixAutoUpdate={false}
     matrixWorldNeedsUpdate={true}
     matrix={props.matrix ?? I}
   >
-    <bufferGeometry attach="geometry"
-      attributes-position={posAttr}
-      attributes-color={colAttr}
-      drawRange={{start: 0, count: msg.linesLength()}}
-    />
+    <bufferGeometry attach="geometry">
+      {positionAttrib}
+      {colorAttrib}
+    </bufferGeometry>
     <lineBasicMaterial attach="material"
       vertexColors={true}
     />
@@ -81,18 +76,22 @@ export function ColoredLinesViewer(props:
 
 export default function VisualizationViewer(props: {namespace: string, topic: string, baseLinkMatrix: Matrix4}) {
   const { namespace, topic, baseLinkMatrix } = props;
-  const [msg, setMsg] = useState<fb.amrl_msgs.VisualizationMsg | null>(null);
+  const [mapMsg, setMapMsg] = useState<fb.amrl_msgs.VisualizationMsg | null>(null);
+  const [baseLinkMsg, setBaseLinkMsg] = useState<fb.amrl_msgs.VisualizationMsg | null>(null);
 
   useRobofleetMsgListener(matchTopic(namespace, topic), useCallback((buf, match) => {
     const msg = fb.amrl_msgs.VisualizationMsg.getRootAsVisualizationMsg(buf);
-    setMsg(msg);
+    const frame = msg.header()?.frameId();
+    if (frame === "map") {
+      setMapMsg(msg);
+    }
+    if (frame === "base_link") {
+      setBaseLinkMsg(msg);
+    }
   }, []));
 
-  if (!msg)
-    return <></>;
-
   return <>
-    <ColoredLinesViewer msg={msg} frame="map"/>
-    <ColoredLinesViewer msg={msg} frame="base_link" matrix={baseLinkMatrix}/>
+    {mapMsg && <ColoredLinesViewer msg={mapMsg}/>}
+    {baseLinkMsg && <ColoredLinesViewer msg={baseLinkMsg} matrix={baseLinkMatrix}/>}
   </>;
 }
