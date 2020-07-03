e30263fbcf411ac13a1fd81a24bd51b0e54d965c
Kavan Sikand <kvsikand@gmail.com>
2020-07-03 10:12:07 -0700

    add buttons, not functional yet

--- src/VizTab.tsx
+++ src/VizTab.tsx
@@ -1,7 +1,7 @@
-import { Box, Checkbox, FormControlLabel, List, Snackbar, Switch } from "@material-ui/core";
+import { Box, Checkbox, makeStyles, createStyles, Theme, FormControlLabel, FormGroup, List, Snackbar, Switch, Button } from "@material-ui/core";
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
 import React, { useCallback, useContext, useState } from "react";
-import { Canvas } from "react-three-fiber";
+import { Canvas, useThree } from "react-three-fiber";
 import * as THREE from "three";
 import CameraControls from "./components/CameraControls";
 import CollapserItem from "./components/CollapserItem";
@@ -19,11 +19,27 @@ function Alert(props: AlertProps) {
   return <MuiAlert elevation={6} variant="standard" {...props}/>;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    mouseDefault: {
+      cursor: 'default'
+    },
+    mouseLocalize: {
+      cursor: 'crosshair'
+    },
+    mouseSetNav: {
+      cursor: 'crosshair'
+    },
+  })
+);
+
 export default function VizTab(props: {namespace: string}) {
   const ws = useContext(WebSocketContext);
   const [locAlertOpen, setLocAlertOpen] = useState(true);
   const [baseLink, setBaseLink] = useState(new THREE.Matrix4());
-
+  const [mouseState, setMouseState] = useState("Default");
+  const classes = useStyles()
+  const { camera } = useThree();
   // build base_link transform using Localization2DMsg
   useRobofleetMsgListener(matchTopic(props.namespace, "localization"), useCallback((buf, match) => {
     const loc = fb.amrl_msgs.Localization2DMsg.getRootAsLocalization2DMsg(buf);
@@ -37,9 +53,65 @@ export default function VizTab(props: {namespace: string}) {
     setLocAlertOpen(false);
   }, []));
 
+  const getMouseClass = () => {
+    if (mouseState == "Default") {
+      return classes.mouseDefault;
+    } else if (mouseState == "Localize") {
+      return classes.mouseLocalize;
+    } else if (mouseState == "SetNav") {
+      return classes.mouseSetNav;
+    } else {
+      return classes.mouseDefault;
+    }
+  }
+
   const handleAlertClose = (event?: React.SyntheticEvent, reason?: string) => {
     if (reason !== "clickaway")
       setLocAlertOpen(false);
+  }
+
+  const clickLocalize = (event?: React.SyntheticEvent) => {
+    if (mouseState == "Localize") {
+      setMouseState("Default");
+    } else {
+      setMouseState("Localize");
+    }
+  }
+
+  const clickSetNav = (event?: React.SyntheticEvent) => {
+    if (mouseState == "SetNav") {
+      setMouseState("Default");
+    } else {
+      setMouseState("SetNav");
+    }
+  }
+
+  const clickCanvas = (event?: React.MouseEvent) => {
+    if (mouseState == "Default") {
+      return;
+    }
+
+    // TODO get click location - fix this? Camera is undefined for reasons
+
+    // let vec = screenToNdc(event!.currentTarget.getBoundingClientRect(), event!.clientX, event!.clientY!);
+    // let worldVec = vec.unproject(camera);
+    // console.log(`CLICKED ${worldVec.x}`);
+
+    if (mouseState == "Localize") {
+      // TODO send pose/localization message to initialpose/set_pose topic
+      
+    } else if (mouseState == "SetNav") {
+      // TODO send pose/localization message to navigation goal topic
+
+    }
+  }
+
+  function screenToNdc(rect: DOMRect, x: number, y: number) {
+    return new THREE.Vector3(
+        ((x - rect.x) / rect.width * 2 - 1),
+        -((y - rect.y) / rect.height * 2 - 1),
+        0
+    );
   }
 
   const [locShowMap, setLocShowMap] = useStorage("Localization.showMap", true);
@@ -50,10 +122,17 @@ export default function VizTab(props: {namespace: string}) {
   const settingsPanel = <SettingsPanel>
     <List >
       <CollapserItem label="Localization">
-        <FormControlLabel
-          control={<Switch checked={locShowMap} onClick={() => setLocShowMap(s => !s)}/>}
-          label="Show map"
-        />
+        <FormGroup row>
+          <FormControlLabel
+            control={<Switch checked={locShowMap} onClick={() => setLocShowMap(s => !s)}/>}
+            label="Show map"
+          />
+        </FormGroup>
+        <FormGroup row>
+          <Button onClick={clickLocalize} variant="outlined">Localize</Button>
+          <Box flexGrow="1" />
+          <Button onClick={clickSetNav}  variant="outlined">Set Nav Goal</Button>
+        </FormGroup>
       </CollapserItem>
       <CollapserItem label="Laser Scan">
         <FormControlLabel
@@ -107,6 +186,8 @@ export default function VizTab(props: {namespace: string}) {
       <Canvas
         orthographic={true}
         pixelRatio={window.devicePixelRatio}
+        onClick={clickCanvas}
+        className={getMouseClass()}
       >
         <CameraControls/>
         {viewers}
