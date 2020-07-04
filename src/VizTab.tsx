8b36e18b816f235834c6e3a7bbd16061d6331d96
Logan Zartman <logan.zartman@utexas.edu>
2020-07-03 19:32:46 -0500

    WIP set pose/localization

--- src/VizTab.tsx
+++ src/VizTab.tsx
@@ -1,7 +1,7 @@
 import { Box, Checkbox, makeStyles, createStyles, Theme, FormControlLabel, FormGroup, List, Snackbar, Switch, Button } from "@material-ui/core";
 import MuiAlert, { AlertProps } from '@material-ui/lab/Alert';
-import React, { useCallback, useContext, useState } from "react";
-import { Canvas, useThree } from "react-three-fiber";
+import React, { useCallback, useContext, useState, useRef } from "react";
+import { Canvas, useThree, ReactThreeFiber } from "react-three-fiber";
 import * as THREE from "three";
 import CameraControls from "./components/CameraControls";
 import CollapserItem from "./components/CollapserItem";
@@ -12,8 +12,75 @@ import VisualizationViewer from "./components/VisualizationViewer";
 import WebSocketContext from "./contexts/WebSocketContext";
 import useRobofleetMsgListener from "./hooks/useRobofleetMsgListener";
 import useStorage from "./hooks/useStorage";
+import { flatbuffers } from "flatbuffers";
 import { fb } from "./schema";
 import { matchTopic } from "./util";
+import CanvasUtils, { CanvasUtilsRef } from "./components/CanvasUtils";
+
+function createPose2DfMsg({namespace, x, y, theta}:
+    {namespace: string, x: number, y: number, theta: number}) {
+  const fbb = new flatbuffers.Builder();
+  const pose = fb.amrl_msgs.Pose2Df.createPose2Df(
+    fbb,
+    fb.MsgMetadata.createMsgMetadata(
+      fbb,
+      fbb.createString("amrl_msgs/Pose2Df"),
+      fbb.createString(`${namespace}/move_base_simple/goal`)
+    ),
+    x,
+    y,
+    theta
+  );
+  fbb.finish(pose);
+  return fbb.asUint8Array();
+}
+
+// oof
+// not sure why we don't get createX() methods for some of these
+function createLocalization2DMsg({namespace, frame="map", map, x, y, theta}:
+    {namespace: string, frame?: string, map: string, x: number, y: number, theta: number}) {
+  const fbb = new flatbuffers.Builder();
+
+  const metadataOffset = fb.MsgMetadata.createMsgMetadata(
+    fbb,
+    fbb.createString("amrl_msgs/Localization2DMsg"),
+    fbb.createString(`${namespace}/initialpose`)
+  );
+
+  const stampOffset = fb.RosTime.createRosTime(
+    fbb,
+    Math.floor(Date.now() / 1000),
+    0
+  );
+
+  const frameOffset = fbb.createString(frame);
+
+  fb.std_msgs.Header.startHeader(fbb);
+  fb.std_msgs.Header.addFrameId(fbb, frameOffset);
+  fb.std_msgs.Header.addSeq(fbb, 0); // TODO idk
+  fb.std_msgs.Header.addStamp(fbb, stampOffset);
+  const headerOffset = fb.std_msgs.Header.endHeader(fbb);
+
+  const mapOffset = fbb.createString(map);
+
+  const poseOffset = fb.amrl_msgs.Pose2Df.createPose2Df(
+    fbb,
+    0,
+    x,
+    y,
+    theta
+  );
+
+  fb.amrl_msgs.Localization2DMsg.startLocalization2DMsg(fbb);
+  fb.amrl_msgs.Localization2DMsg.add_Metadata(fbb, metadataOffset);
+  fb.amrl_msgs.Localization2DMsg.addHeader(fbb, headerOffset);
+  fb.amrl_msgs.Localization2DMsg.addMap(fbb, mapOffset);
+  fb.amrl_msgs.Localization2DMsg.addPose(fbb, poseOffset);
+  const loc = fb.amrl_msgs.Localization2DMsg.endLocalization2DMsg(fbb);
+
+  fbb.finish(loc);
+  return fbb.asUint8Array();
+}
 
 function Alert(props: AlertProps) {
   return <MuiAlert elevation={6} variant="standard" {...props}/>;
@@ -21,25 +88,34 @@ function Alert(props: AlertProps) {
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    mouseDefault: {
+    Default: {
       cursor: 'default'
     },
-    mouseLocalize: {
+    Localize: {
       cursor: 'crosshair'
     },
-    mouseSetNav: {
+    SetNav: {
       cursor: 'crosshair'
     },
   })
 );
 
 export default function VizTab(props: {namespace: string}) {
+  type ClickAction = "Default" | "Localize" | "SetNav";
+
   const ws = useContext(WebSocketContext);
+  const canvasUtils = useRef<CanvasUtilsRef>(null);
+  const classes = useStyles();
+
   const [locAlertOpen, setLocAlertOpen] = useState(true);
   const [baseLink, setBaseLink] = useState(new THREE.Matrix4());
-  const [mouseState, setMouseState] = useState("Default");
-  const classes = useStyles()
-  const { camera } = useThree();
+  const [clickAction, setClickAction] = useState<ClickAction>("Default");
+
+  const [locShowMap, setLocShowMap] = useStorage("Localization.showMap", true);
+  const [scanShow, setScanShow] = useStorage("LaserScan.show", true);
+  const [vizShowPoints, setVizShowPoints] = useStorage("Viz.showPoints", false);
+  const [vizShowLines, setVizShowLines] = useStorage("Viz.showLines", false);
+
   // build base_link transform using Localization2DMsg
   useRobofleetMsgListener(matchTopic(props.namespace, "localization"), useCallback((buf, match) => {
     const loc = fb.amrl_msgs.Localization2DMsg.getRootAsLocalization2DMsg(buf);
@@ -53,72 +129,41 @@ export default function VizTab(props: {namespace: string}) {
     setLocAlertOpen(false);
   }, []));
 
-  const getMouseClass = () => {
-    if (mouseState == "Default") {
-      return classes.mouseDefault;
-    } else if (mouseState == "Localize") {
-      return classes.mouseLocalize;
-    } else if (mouseState == "SetNav") {
-      return classes.mouseSetNav;
-    } else {
-      return classes.mouseDefault;
-    }
-  }
-
-  const handleAlertClose = (event?: React.SyntheticEvent, reason?: string) => {
-    if (reason !== "clickaway")
-      setLocAlertOpen(false);
-  }
-
-  const clickLocalize = (event?: React.SyntheticEvent) => {
-    if (mouseState == "Localize") {
-      setMouseState("Default");
-    } else {
-      setMouseState("Localize");
-    }
-  }
-
-  const clickSetNav = (event?: React.SyntheticEvent) => {
-    if (mouseState == "SetNav") {
-      setMouseState("Default");
-    } else {
-      setMouseState("SetNav");
-    }
-  }
-
-  const clickCanvas = (event?: React.MouseEvent) => {
-    if (mouseState == "Default") {
+  const clickCanvas = () => {
+    if (clickAction === "Default") {
       return;
     }
+    if (clickAction === "Localize") {
+      const pos = canvasUtils.current?.worldMousePos;
 
-    // TODO get click location - fix this? Camera is undefined for reasons
+      if (ws?.connected) {
+        ws.ws?.send(createLocalization2DMsg({
+          namespace: props.namespace,
+          frame: "map",
+          map: "n/a", // TODO: map selector?
+          x: pos?.x ?? 0,
+          y: pos?.y ?? 0,
+          theta: 0 // TODO: implement drag to set angle
+        }));
+      }
+    } 
+    if (clickAction === "SetNav") {
+      const pos = canvasUtils.current?.worldMousePos;
 
-    // let vec = screenToNdc(event!.currentTarget.getBoundingClientRect(), event!.clientX, event!.clientY!);
-    // let worldVec = vec.unproject(camera);
-    // console.log(`CLICKED ${worldVec.x}`);
-
-    if (mouseState == "Localize") {
-      // TODO send pose/localization message to initialpose/set_pose topic
-      
-    } else if (mouseState == "SetNav") {
-      // TODO send pose/localization message to navigation goal topic
-
+      if (ws?.connected) { 
+        ws.ws?.send(createPose2DfMsg({
+          namespace: props.namespace,
+          x: pos?.x ?? 0,
+          y: pos?.y ?? 0,
+          theta: 0 // TODO: implement drag to set angle
+        }));
+      }
+      // TODO: remove (fix Z coord)
+      console.log(pos);
     }
   }
 
-  function screenToNdc(rect: DOMRect, x: number, y: number) {
-    return new THREE.Vector3(
-        ((x - rect.x) / rect.width * 2 - 1),
-        -((y - rect.y) / rect.height * 2 - 1),
-        0
-    );
-  }
-
-  const [locShowMap, setLocShowMap] = useStorage("Localization.showMap", true);
-  const [scanShow, setScanShow] = useStorage("LaserScan.show", true);
-  const [vizShowPoints, setVizShowPoints] = useStorage("Viz.showPoints", false);
-  const [vizShowLines, setVizShowLines] = useStorage("Viz.showLines", false);
-
+  // settings panel and FAB
   const settingsPanel = <SettingsPanel>
     <List >
       <CollapserItem label="Localization">
@@ -129,9 +174,19 @@ export default function VizTab(props: {namespace: string}) {
           />
         </FormGroup>
         <FormGroup row>
-          <Button onClick={clickLocalize} variant="outlined">Localize</Button>
+          <Button 
+            onClick={() => setClickAction(a => (a === "Localize") ? "Default" : "Localize")}
+            variant="outlined"
+          >
+            Set Pose
+          </Button>
           <Box flexGrow="1" />
-          <Button onClick={clickSetNav}  variant="outlined">Set Nav Goal</Button>
+          <Button 
+            onClick={() => setClickAction(a => (a === "SetNav") ? "Default" : "SetNav")}
+            variant="outlined"
+          >
+            Set NavGoal
+          </Button>
         </FormGroup>
       </CollapserItem>
       <CollapserItem label="Laser Scan">
@@ -153,8 +208,7 @@ export default function VizTab(props: {namespace: string}) {
     </List>
   </SettingsPanel>;
 
-  // just extract the controls to here and pass in props
-
+  // these viewers will be rendered into the <Canvas>
   // since <Canvas> uses the react-three-fiber reconciler, we must forward
   // any contexts manually :(
   const viewers = <WebSocketContext.Provider value={ws}>
@@ -181,20 +235,27 @@ export default function VizTab(props: {namespace: string}) {
       />
   </WebSocketContext.Provider>;
 
+  // only close a snackbar if the close button was pressed
+  const handleSnackbarClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason !== "clickaway")
+      setLocAlertOpen(false);
+  }
+
   return <>
     <Box position="absolute" zIndex="-1" bottom="0" top="0" left="0" right="0">
       <Canvas
         orthographic={true}
         pixelRatio={window.devicePixelRatio}
         onClick={clickCanvas}
-        className={getMouseClass()}
+        className={classes[clickAction]}
       >
+        <CanvasUtils ref={canvasUtils}/>
         <CameraControls/>
         {viewers}
       </Canvas>
     </Box>
-    <Snackbar open={locAlertOpen} onClose={handleAlertClose}>
-      <Alert severity="warning" onClose={handleAlertClose}>
+    <Snackbar open={locAlertOpen} onClose={handleSnackbarClose}>
+      <Alert severity="warning" onClose={handleSnackbarClose}>
         No localization available. Coordinate frames may be incorrect.
       </Alert>
     </Snackbar>
---
+++ src/components/CanvasUtils.tsx
@@ -0,0 +1,69 @@
+import { useThree } from "react-three-fiber";
+import { Vector3 } from "three";
+import React, { useImperativeHandle, Ref, useCallback, useEffect, useRef } from "react";
+
+export interface CanvasUtilsProps {
+};
+export interface CanvasUtilsRef {
+  worldMousePos: Vector3
+};
+
+/**
+ * A utility component for exporting information from a react-three-fiber
+ * Canvas. Consume by rendering it inside a <Canvas> and attaching a ref, 
+ * then imperatively accessing properties on the ref.
+ */
+function CanvasUtils(props: CanvasUtilsProps, ref: Ref<CanvasUtilsRef>) {
+  const three = useThree();
+  if (!three)
+    throw new Error("CanvasUtils must be rendered inside a react-three-fiber <Canvas> component.");
+  
+  const worldPos = useRef(new Vector3());
+  const pointerMoveEvent = useRef<PointerEvent | null>(null);
+
+  // https://github.com/react-spring/react-three-fiber/blob/889e5734c5e64c951bde6d9b0b042f99449c3c15/src/canvas.tsx#L290
+  // https://github.com/loganzartman/f1tenth-webviz/blob/master/src/index.js#L320
+  const getNdcMousePos = () => {
+    const x = pointerMoveEvent.current?.clientX ?? 0;
+    const y = pointerMoveEvent.current?.clientY ?? 0;
+    const { left, right, top, bottom } = three.size;
+    return new Vector3(
+        ((x - left) / (right - left) * 2 - 1),
+        -((y - top) / (bottom - top) * 2 - 1),
+        0
+    );
+  }
+
+  // react-three-fiber has lots of built-in things for mouse events on objects
+  // that are raycast-able, but we want *all* move events.
+  useEffect(() => {
+    const handler = (event: PointerEvent) => {
+      pointerMoveEvent.current = event;
+    }
+    document.body.addEventListener("pointermove", handler);
+    return () => document.body.removeEventListener("pointermove", handler);
+  }, []);
+  
+  // required reading:
+  // https://reactjs.org/docs/hooks-reference.html#useimperativehandle
+  useImperativeHandle(ref, useCallback(() => {
+    // you can add more things inside this object
+    // you will have to update the interface definition
+    const refObj: CanvasUtilsRef = {
+      get worldMousePos() {
+        return getNdcMousePos().unproject(three.camera);
+      }
+    };
+    return refObj;
+  }, [worldPos.current]));
+  
+  // just using this to element to render inside of <Canvas>, no need to
+  // generate anything useful here
+  return <group 
+    name="CanvasUtils dummy" 
+    ref={ref}
+  />;
+}
+
+// see required reading above
+export default React.forwardRef(CanvasUtils);
