9710c924c7de41748dbb4c925168427ab9fd7338
Kavan Sikand <kvsikand@gmail.com>
2020-09-09 12:00:31 -0500

    add static robot listing to overview

--- src/Overview.tsx
+++ src/Overview.tsx
@@ -11,9 +11,17 @@ import {
   TableHead,
   TableRow,
   Typography,
+  makeStyles,
+  Theme,
 } from '@material-ui/core';
 import { Check, Clear } from '@material-ui/icons';
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  ReactElement,
+} from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import PercentageDisplay from './components/PercentageDisplay';
@@ -21,6 +29,9 @@ import AppContext from './contexts/AppContext';
 import useRobofleetMsgListener from './hooks/useRobofleetMsgListener';
 import { fb } from './schema';
 import { matchTopicAnyNamespace } from './util';
+import config from './config';
+import IdTokenContext from './contexts/IdTokenContext';
+import moment from 'moment';
 
 type RobotStatus = {
   name: string;
@@ -28,11 +39,29 @@ type RobotStatus = {
   battery_level: number;
   status: string;
   location: string;
+  is_active: boolean;
+  last_updated: string;
 };
+
+type StaticRobotInfo = {
+  name: string;
+  ip: string;
+  lastStatus: string;
+  lastLocation: string;
+  lastUpdated: string;
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+  inactive: {
+    backgroundColor: theme.palette.background.default,
+  },
+}));
 
 export default function Overview() {
   const { setPaused } = useContext(AppContext);
+  const { idToken } = useContext(IdTokenContext);
   const [data, setData] = useState({} as { [name: string]: RobotStatus });
+  const classes = useStyles();
 
   useEffect(() => {
     setPaused(false);
@@ -51,15 +80,68 @@ export default function Overview() {
           battery_level: status.batteryLevel(),
           status: status.status() ?? '',
           location: status.location() ?? '',
+          is_active: true,
+          last_updated: 'now',
         },
       }));
     }, [])
   );
 
+  useEffect(() => {
+    (async () => {
+      const baseUrl = new URL(config.serverUrl);
+      baseUrl.protocol = window.location.protocol;
+      const url = new URL('robots', baseUrl).toString();
+      const res = await fetch(url, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      let robotInfo = await res.json();
+      robotInfo.forEach((robot: StaticRobotInfo) => {
+        const name = '/' + robot.name;
+        if (!data[name]) {
+          // don't overwrite any live robot data with this static info
+          setData((data) => ({
+            ...data,
+            [name]: {
+              name: name,
+              is_ok: true,
+              battery_level: 0,
+              status: robot.lastStatus,
+              location: robot.lastLocation,
+              is_active: false,
+              last_updated: moment(robot.lastUpdated).fromNow(),
+            },
+          }));
+        }
+      });
+    })();
+  }, [data]);
+
   const items = Object.entries(data).map(([name, obj]) => {
-    const href = `/robot/${btoa(name)}`;
+    let detailsContent: ReactElement | string;
+    if (obj.is_active) {
+      const href = `/robot/${btoa(name)}`;
+      detailsContent = (
+        <Button
+          component={Link}
+          to={href}
+          size="small"
+          variant="outlined"
+          color="primary"
+        >
+          View
+        </Button>
+      );
+    } else {
+      detailsContent = `Last Seen: \n ${obj.last_updated}`;
+    }
+
     return (
-      <TableRow key={name}>
+      <TableRow key={name} className={obj.is_active ? '' : classes.inactive}>
         <TableCell align="left">{name}</TableCell>
         <TableCell align="center">
           {obj.is_ok ? <Check /> : <Clear color="error" />}
@@ -69,16 +151,8 @@ export default function Overview() {
         </TableCell>
         <TableCell align="center">{obj.status}</TableCell>
         <TableCell align="center">{obj.location}</TableCell>
-        <TableCell align="center">
-          <Button
-            component={Link}
-            to={href}
-            size="small"
-            variant="outlined"
-            color="primary"
-          >
-            View
-          </Button>
+        <TableCell style={{ whiteSpace: 'pre-line' }} align="center">
+          {detailsContent}
         </TableCell>
       </TableRow>
     );
@@ -109,7 +183,10 @@ export default function Overview() {
                 </TableCell>
                 <TableCell align="center">Status</TableCell>
                 <TableCell align="center">Location</TableCell>
-                <TableCell style={{ width: '80px' }} align="center"></TableCell>
+                <TableCell
+                  style={{ width: '120px' }}
+                  align="center"
+                ></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>{items}</TableBody>
