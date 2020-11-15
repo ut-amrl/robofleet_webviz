b9a3a0c095d8c63b87bd30e2bb88e44f68f76325
Logan Zartman <logan.zartman@utexas.edu>
2020-11-14 18:27:11 -0600

    new battery indicator

--- src/components/PercentageDisplay.tsx
+++ src/components/PercentageDisplay.tsx
@@ -1,53 +1,48 @@
 import Box from '@material-ui/core/Box';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
 import React from 'react';
 
+const barColor = (value: number, opacity: number) => {
+  const f = Math.round(value * 255);
+  return `rgba(${255 - f}, ${f}, 0, ${opacity})`;
+};
+
 const useStyles = makeStyles({
-  root: (props: { value: number }) => {
-    const f = Math.round(props.value * 255);
+  root: () => {
     return {
-      color: `rgba(${255 - f}, ${f}, 50, 0.5)`,
+      height: 8,
     };
   },
+  colorPrimary: (props: { value: number }) => ({
+    backgroundColor: barColor(props.value, 0.2),
+  }),
+  barColorPrimary: (props: { value: number }) => ({
+    backgroundColor: barColor(props.value, 1.0),
+  }),
 });
 
 export default function PercentageDisplay(props: { value: number }) {
   const classes = useStyles({ value: props.value });
+  const { value: originalValue, ...otherProps } = props;
+  const value = originalValue * 100;
 
   return (
-    <Box position="relative" display="inline-flex">
-      <CircularProgress
-        variant="static"
-        className={classes.root}
-        value={props.value * 100}
-      />
-      <Box
-        top={0}
-        left={0}
-        bottom={0}
-        right={0}
-        position="absolute"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Typography
-          variant="caption"
-          component="div"
-          color="textSecondary"
-        >{`${Math.round(props.value * 100)}%`}</Typography>
+    <Box display="flex" alignItems="center">
+      <Box width="100%" mr={0.5}>
+        <LinearProgress
+          classes={classes}
+          variant="determinate"
+          value={value}
+          {...otherProps}
+        />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="textPrimary">{`${Math.round(
+          value
+        )}%`}</Typography>
       </Box>
     </Box>
   );
 }
-
-PercentageDisplay.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate and static variants.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
-};
