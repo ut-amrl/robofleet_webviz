4f9838e23ade782472b8e6415fb268e38fd4d821
kvsikand <kvsikand@gmail.com>
2021-03-17 19:28:14 -0500

    Kavan/robot management (#32)
    * add manage+remove buttons
    
    * update deletion logic
    
    * update removal logic
    
    Co-authored-by: Kavan S <amrl_user@robofleet.csres.utexas.edu>

--- src/Overview.tsx
+++ src/Overview.tsx
@@ -14,7 +14,7 @@ import {
   makeStyles,
   Theme,
 } from '@material-ui/core';
-import { Check, Clear } from '@material-ui/icons';
+import { Check, Clear, Delete } from '@material-ui/icons';
 import CloudOff from '@material-ui/icons/CloudOff';
 import React, {
   useCallback,
@@ -34,6 +34,8 @@ import { matchTopicAnyNamespace } from './util';
 import config from './config';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import IdTokenContext from './contexts/IdTokenContext';
+
 dayjs.extend(relativeTime);
 
 type RobotStatus = {
@@ -94,7 +96,74 @@ const MaybeDisconnectedLabel = (props: {
 export default function Overview() {
   const { setPaused } = useContext(AppContext);
   const [data, setData] = useState({} as { [name: string]: RobotStatus });
+  const [manageMode, setManageMode] = useState(false);
+  const { idToken } = useContext(IdTokenContext);
   const classes = useStyles();
+
+  const updateRobotList = useCallback(async () => {
+    const baseUrl = new URL(config.serverUrl);
+    baseUrl.protocol = window.location.protocol;
+    const url = new URL('robots', baseUrl).toString();
+    const res = await fetch(url, {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (res.ok) {
+      try {
+        const robotInfo = (await res.json()) as Array<StaticRobotInfo>;
+        const staticData = {} as { [name: string]: RobotStatus };
+        robotInfo.forEach((robot) => {
+          const name = '/' + robot.name;
+          staticData[name] = {
+            name,
+            is_ok: true,
+            battery_level: -1,
+            status: robot.lastStatus,
+            location: robot.lastLocation,
+            is_active: false,
+            last_updated: dayjs(robot.lastUpdated).fromNow(),
+          };
+        });
+
+        setData({
+          ...staticData,
+          ...data,
+        });
+      } catch (err) {
+        console.error(`Failed to fetch static robot info`, err);
+      }
+    }
+  }, [data]);
+
+  async function handleRemove(name: string, obj: any) {
+    console.log('removing ', name, obj);
+    const baseUrl = new URL(config.serverUrl);
+    baseUrl.protocol = window.location.protocol;
+    const url = new URL('robots/delete', baseUrl).toString();
+
+    try {
+      await fetch(url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          ...(idToken && { id_token: idToken }),
+        }),
+      });
+
+      const newData = data;
+      delete newData[name];
+
+      setData(newData);
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   useEffect(() => {
     setPaused(false);
@@ -102,61 +171,32 @@ export default function Overview() {
 
   useRobofleetMsgListener(
     matchTopicAnyNamespace('status'),
-    useCallback((buf, match) => {
-      const name = match[1];
-      const status = fb.amrl_msgs.RobofleetStatus.getRootAsRobofleetStatus(buf);
-      setData((data) => ({
-        ...data,
-        [name]: {
-          name: name,
-          is_ok: status.isOk(),
-          battery_level: status.batteryLevel(),
-          status: status.status() ?? '',
-          location: status.location() ?? '',
-          is_active: true,
-          last_updated: 'now',
-        },
-      }));
-    }, [])
+    useCallback(
+      (buf, match) => {
+        const name = match[1];
+        const status = fb.amrl_msgs.RobofleetStatus.getRootAsRobofleetStatus(
+          buf
+        );
+        setData({
+          ...data,
+          [name]: {
+            name: name,
+            is_ok: status.isOk(),
+            battery_level: status.batteryLevel(),
+            status: status.status() ?? '',
+            location: status.location() ?? '',
+            is_active: true,
+            last_updated: 'now',
+          },
+        });
+      },
+      [data]
+    )
   );
 
   useEffect(() => {
-    (async () => {
-      const baseUrl = new URL(config.serverUrl);
-      baseUrl.protocol = window.location.protocol;
-      const url = new URL('robots', baseUrl).toString();
-      const res = await fetch(url, {
-        method: 'get',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (res.ok) {
-        try {
-          const robotInfo = (await res.json()) as Array<StaticRobotInfo>;
-          robotInfo.forEach((robot) => {
-            const name = '/' + robot.name;
-            setData((data) => ({
-              [name]: {
-                name,
-                is_ok: true,
-                battery_level: -1,
-                status: robot.lastStatus,
-                location: robot.lastLocation,
-                is_active: false,
-                last_updated: dayjs(robot.lastUpdated).fromNow(),
-              },
-              // don't overwrite any live robot data with this static info
-              ...data,
-            }));
-          });
-        } catch (err) {
-          console.error(`Failed to fetch static robot info`, err);
-        }
-      }
-    })();
-  }, []);
+    updateRobotList();
+  }, [updateRobotList]);
 
   const tableHead = (
     <TableHead>
@@ -171,6 +211,7 @@ export default function Overview() {
         <TableCell align="center">Status</TableCell>
         <TableCell align="center">Location</TableCell>
         <TableCell align="center">Last seen</TableCell>
+        {manageMode ? <TableCell align="center"></TableCell> : null}
       </TableRow>
     </TableHead>
   );
@@ -205,6 +246,17 @@ export default function Overview() {
       </Button>
     );
 
+    const removalContent = obj.is_active ? null : (
+      <Button
+        onClick={() => handleRemove(name, obj)}
+        style={{ textTransform: 'none' }}
+        variant={'outlined'}
+        startIcon={<Delete />}
+      >
+        Remove
+      </Button>
+    );
+
     return (
       <TableRow key={name} className={obj.is_active ? '' : classes.inactive}>
         <TableCell align="left">{nameContent}</TableCell>
@@ -225,6 +277,9 @@ export default function Overview() {
           />
         </TableCell>
         <TableCell align="center">{obj.last_updated}</TableCell>
+        {manageMode ? (
+          <TableCell align="center">{removalContent}</TableCell>
+        ) : null}
       </TableRow>
     );
   });
@@ -234,13 +289,29 @@ export default function Overview() {
       <NavBar />
       <Box height="2em" />
       <Container component="main" maxWidth="md">
-        <Typography
-          variant="h4"
-          component="h2"
-          style={{ marginBottom: '0.25em' }}
+        <Box
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
         >
-          Robots
-        </Typography>
+          <Typography
+            variant="h4"
+            component="h2"
+            style={{ marginBottom: '0.25em' }}
+          >
+            Robots
+          </Typography>
+          <Button
+            style={{ marginBottom: '1em' }}
+            variant="contained"
+            color={manageMode ? 'primary' : 'default'}
+            onClick={(e) => setManageMode(!manageMode)}
+          >
+            Manage
+          </Button>
+        </Box>
         <TableContainer component={Paper}>
           <Table>
             {tableHead}
